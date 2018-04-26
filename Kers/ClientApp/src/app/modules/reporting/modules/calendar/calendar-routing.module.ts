fc72400fef0e648cf94d1ec14b87aaec5fed5651
Ivelin Denev <idenev@extension.org>
2018-04-26 10:08:14 -0400

    Major Programs Switch when fiscal yer changes

--- Kers/ClientApp/src/app/modules/reporting/modules/calendar/calendar-routing.module.ts
+++ Kers/ClientApp/src/app/modules/reporting/modules/calendar/calendar-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { KersCalendarComponent } from './kers-calendar.component';
+import { CalendarHomeComponent } from './calendar-home.component';
 
 const routes: Routes = [
   {
@@ -8,7 +9,7 @@ const routes: Routes = [
       children: [
         {
           path: '',
-          component: KersCalendarComponent
+          component: CalendarHomeComponent
         } 
       ]
 
