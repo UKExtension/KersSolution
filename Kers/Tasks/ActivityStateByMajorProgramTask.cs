2128d57810b345691e5232b2654126f08497a750
Denev <idene3@uky.edu>
2018-07-12 12:06:19 -0400

    Fix some links in reports

--- Kers/Tasks/ActivityStateByMajorProgramTask.cs
+++ Kers/Tasks/ActivityStateByMajorProgramTask.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Kers.Models.Contexts;
 using Kers.Models.Entities.KERScore;
 using Kers.Models.Repositories;
+using Kers.Models.ViewModels;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,9 +39,18 @@ namespace Kers.Tasks
                     var fiscalYearRepo = new FiscalYearRepository( context );
                     var repo = new ActivityRepository(cache, context);
                     var startTime = DateTime.Now;
-                    var str = await repo.StateByMajorProgram(fiscalYearRepo.currentFiscalYear(FiscalYearType.ServiceLog), 0, true);
-                    str = await repo.StateByMajorProgram(fiscalYearRepo.currentFiscalYear(FiscalYearType.ServiceLog), 1, true);
-                    str = await repo.StateByMajorProgram(fiscalYearRepo.currentFiscalYear(FiscalYearType.ServiceLog), 2, true);
+
+                    var years = fiscalYearRepo.GetAll();
+
+                    years = years.Where( y => y.Type == FiscalYearType.ServiceLog && y.Start < DateTime.Now );
+                    TableViewModel str = null;
+                    foreach( var year in years){
+                        str = await repo.StateByMajorProgram(year, 0, true);
+                        str = await repo.StateByMajorProgram(year, 1, true);
+                        str = await repo.StateByMajorProgram(year, 2, true);
+                    }
+
+                    
                     var endTime = DateTime.Now;
                     await LogComplete(context, 
                                     "TableViewModel", str, 
