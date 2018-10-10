afa02892b541eca7daf08549ca7de69992bbff31
Ivelin Denev <idenev@extension.org>
2018-10-10 09:11:29 -0400

    Display by default current fiscal year in stats

--- Kers/ClientApp/src/app/modules/reporting/modules/contact/contact.module.ts
+++ Kers/ClientApp/src/app/modules/reporting/modules/contact/contact.module.ts
@@ -23,6 +23,8 @@ import {ContactStatsProgramhRow} from './stats/contact-stats-program-row.compone
 
 
 import {ContactService} from './contact.service';
+import { ActivityStatsRow } from '../activity/stats/activity-stats-row.component';
+import { ActivityStatsMonthRow } from '../activity/stats/activity-stats-month-row.component';
  
 
 
@@ -46,7 +48,9 @@ import {ContactService} from './contact.service';
                     ContactStatsMonthComponent,
                     ContactStatsMonthRow,
                     ContactStatsProgramComponent,
-                    ContactStatsProgramhRow
+                    ContactStatsProgramhRow,
+                    ActivityStatsRow,
+                    ActivityStatsMonthRow
                  
                 ],
   providers:    [  
--- Kers/ClientApp/src/app/modules/reporting/modules/contact/stats/contact-stats-month.component.ts
+++ Kers/ClientApp/src/app/modules/reporting/modules/contact/stats/contact-stats-month.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import { ActivityService, Race, ActivityOptionNumber } from '../../activity/activity.service';
+import { Component, Input } from '@angular/core';
+import { ActivityService, Race, ActivityOptionNumber, Activity } from '../../activity/activity.service';
 import {ContactService, Contact} from '../contact.service';
 
 import { Router } from "@angular/router";
 import { Observable } from "rxjs/Observable";
+import { FiscalYear } from '../../admin/fiscalyear/fiscalyear.service';
+import { User } from '../../user/user.service';
 
 
 @Component({
   templateUrl: 'contact-stats-month.component.html'
 })
 export class ContactStatsMonthComponent { 
-
+    @Input() user:User;
 
     errorMessage: string;
 
@@ -27,12 +29,65 @@ export class ContactStatsMonthComponent {
 
     ngOnInit(){
         
-
-        this.activities = this.contactService.summaryPerMonth();
         this.races = this.service.races();
         this.optionNumbers = this.service.optionnumbers();
         
         
+    }
+
+    fiscalYearSwitched(event:FiscalYear){
+        if(this.user == null){
+            this.activities = this.service.summaryPerMonth(0,event.name);
+        }else{
+            this.activities = this.service.summaryPerMonth(this.user.id, event.name);
+        }
+    }
+
+    totalHours( actvts:Activity[]){
+        var total = actvts.map( a => a.hours).reduce( (one, two) => one + two);
+        return total;
+    }
+    totalMultistate( actvts){
+        var total = actvts.map( a => a.multistate).reduce( (one, two) => one + two);
+        return total;
+    }
+    totalContacts( actvts){
+        var total = actvts.map( a => a.males + a.females).reduce( (one, two) => one + two);
+        return total;
+    }
+    raceValue(race:Race, actvts){
+
+        var filtered = actvts.map( a => a.raceEthnicityValues );
+        filtered = [].concat.apply([], filtered);
+        filtered = filtered.filter( a => a.raceId== race.id);
+        var total = filtered.map( r => r.amount).reduce( (one, two) => one + two);
+        return total;
+    }
+
+    ethnicityValue( ethnicityId, actvts){
+
+        var filtered = actvts.map( a => a.raceEthnicityValues );
+        filtered = [].concat.apply([], filtered);
+        filtered = filtered.filter( a => a.ethnicityId== ethnicityId);
+        var total = filtered.map( r => r.amount).reduce( (one, two) => one + two);
+        return total;
+    }
+    totalMales( actvts){
+        var total = actvts.map( a => a.males ).reduce( (one, two) => one + two);
+        return total;
+    }
+    totalFemales( actvts){
+        var total = actvts.map( a => a.females).reduce( (one, two) => one + two);
+        return total;
+    }
+
+    optionNumberValue( optionNumber:ActivityOptionNumber, actvts){
+
+        var filtered = actvts.map( a => a.optionNumberValues );
+        filtered = [].concat.apply([], filtered);
+        filtered = filtered.filter( a => a.activityOptionNumberId== optionNumber.id);
+        var total = filtered.map( r => r.value).reduce( (one, two) => one + two);
+        return total;
     }
 
 
