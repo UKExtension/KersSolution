7eb7e76380f866add25db352b809135b580f84bd
Ivelin Denev <idenev@extension.org>
2018-11-07 10:09:22 -0500

    Add created and modified datatime

--- KersDb/Entities/KERScore/ExtensionEvent.cs
+++ KersDb/Entities/KERScore/ExtensionEvent.cs
@@ -135,6 +135,19 @@ namespace Kers.Models.Entities.KERScore
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "onlineMeetingUrl", Required = Newtonsoft.Json.Required.Default)]
         public string OnlineMeetingUrl { get; set; }
 
+        /// <summary>
+        /// Gets or sets created date time.
+        /// The Timestamp type represents date and time information using ISO 8601 format and is always in UTC time. For example, midnight UTC on Jan 1, 2014 would look like this: '2014-01-01T00:00:00Z'
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "createdDateTime", Required = Newtonsoft.Json.Required.Default)]
+        public DateTime? CreatedDateTime { get; set; }
+    
+        /// <summary>
+        /// Gets or sets last modified date time.
+        /// The Timestamp type represents date and time information using ISO 8601 format and is always in UTC time. For example, midnight UTC on Jan 1, 2014 would look like this: '2014-01-01T00:00:00Z'
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "lastModifiedDateTime", Required = Newtonsoft.Json.Required.Default)]
+        public DateTime? LastModifiedDateTime { get; set; }
 
     
     }
