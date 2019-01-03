172cfccf275bac11a20d1ae5357096d547a99628
Ivelin Denev <idenev@extension.org>
2019-01-03 12:38:14 -0500

    Serve Spa filer from the dir they are published

--- Kers/Startup.cs
+++ Kers/Startup.cs
@@ -186,7 +186,7 @@ namespace Kers
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "ClientApp/dist/ClientApp";
             });
         }
 
