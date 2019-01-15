c7624ec1ba65e8333296a0ee84f2502f1c950c46
Ivelin Denev <idenev@extension.org>
2019-01-15 17:31:00 -0500

    Upgrade SnapEd service to ng7

--- Kers/ClientApp/src/app/modules/reporting/modules/servicelog/snaped.service.ts
+++ Kers/ClientApp/src/app/modules/reporting/modules/servicelog/snaped.service.ts
@@ -1,146 +1,136 @@
 import { Injectable} from '@angular/core';
 import {Location} from '@angular/common';
-import {Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/of';
-import {AuthHttp} from '../../../authentication/auth.http';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpErrorHandler, HandleError } from '../../core/services/http-error-handler.service';
 import {Servicelog} from './servicelog.service';
 
 @Injectable()
 export class SnapedService {
 
     private baseUrl = '/api/Snaped/';
-
-
+    private handleError: HandleError;
 
 
     constructor( 
-        private http:AuthHttp, 
-        private location:Location
-        ){}
+        private http: HttpClient, 
+        private location:Location,
+        httpErrorHandler: HttpErrorHandler
+        ) {
+            this.handleError = httpErrorHandler.createHandleError('SnapedService');
+        }
 
-
-
-
-
-    latest(skip:number = 0, take:number = 5){
+    latest(skip:number = 0, take:number = 5):Observable<Servicelog[]>{
         var url = this.baseUrl + 'latest/' + skip + '/' + take;
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <Servicelog[]>res.json() )
-                .catch(this.handleError);
+        return this.http.get<Servicelog[]>(this.location.prepareExternalUrl(url))
+            .pipe(
+                catchError(this.handleError('latest', []))
+            );
     }
 
-    reportedhours(id:number = 0){
+    reportedhours(id:number = 0):Observable<number>{
         var url = this.baseUrl + 'reportedHours/' + id;
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <number>res.json() )
-                .catch(this.handleError);
+        return this.http.get<number>(this.location.prepareExternalUrl(url))
+            .pipe(
+                catchError(this.handleError('reportedhours', 0))
+            );
     }
 
-    committedhours(id:number = 0){
+    committedhours(id:number = 0):Observable<number>{
         var url = this.baseUrl + 'committed/' + id;
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <number>res.json() )
-                .catch(this.handleError);
+        return this.http.get<number>(this.location.prepareExternalUrl(url))
+            .pipe(
+                catchError(this.handleError('committedhours', 0))
+            );
     }
 
-    statsPerIndividual(userId:number, fy:string = "0"){
+    statsPerIndividual(userId:number, fy:string = "0"):Observable<{}>{
         var url = this.baseUrl + 'userstats/' + userId + '/' + fy;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('statsPerIndividual'))
+            );
     }
 
-    commitmentPerIndividual(userId:number, fy:string = "0"){
+    commitmentPerIndividual(userId:number, fy:string = "0"):Observable<{}>{
         var url = this.baseUrl + 'commitments/' + userId + '/' + fy;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('commitmentPerIndividual'))
+            );
     }
 
-
-
-
-    reportedhoursCounty(id:number){
+    reportedhoursCounty(id:number):Observable<number>{
         var url = this.baseUrl + 'reportedhourscounty';
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <number>res.json() )
-                .catch(this.handleError);
+        return this.http.get<number>(this.location.prepareExternalUrl(url))
+            .pipe(
+                catchError(this.handleError('reportedhoursCounty', 0))
+            );
     }
 
-    committedhoursCounty(id:number = 0){
+    committedhoursCounty(id:number = 0):Observable<number>{
         var url = this.baseUrl + 'committedhourscounty/' + id;
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <number>res.json() )
-                .catch(this.handleError);
+        return this.http.get<number>(this.location.prepareExternalUrl(url))
+                .pipe(
+                    catchError(this.handleError('committedhoursCounty', 0))
+                );
     }
 
-    statsPerCounty(countyId:number){
+    statsPerCounty(countyId:number):Observable<{}>{
         var url = this.baseUrl + 'reportedcounty/' + countyId;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('statsPerCounty'))
+            );
     }
 
-    commitmentPerCounty(countyId:number){
+    commitmentPerCounty(countyId:number):Observable<{}>{
         var url = this.baseUrl + 'committedcounty/' + countyId;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('commitmentPerCounty'))
+            );
     }
-
-
-
-
 
     comitmentActivityTypes(fiscalyearid:number = 0){
         var url = this.baseUrl + 'activitytypes' + '/' + fiscalyearid;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('comitmentActivityTypes'))
+            );
     }
 
     comitmentProjectTypes(fiscalyearid:number = 0){
         var url = this.baseUrl + 'projecttypes' + '/' + fiscalyearid;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('comitmentProjectTypes'))
+            );
     }
+
     comitmentReinforcementItems(fiscalyearid:number = 0){
         var url = this.baseUrl + 'reinforcementitems' + '/' + fiscalyearid;
         return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => res.json() )
-                .catch(this.handleError);
+            .pipe(
+                catchError(this.handleError('comitmentReinforcementItems'))
+            );
     }
 
-    copies(){
+    copies():Observable<number>{
         var url = this.baseUrl + 'copies';
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <number>res.json() )
-                .catch(this.handleError);
+        return this.http.get<number>(this.location.prepareExternalUrl(url))
+            .pipe(
+                catchError(this.handleError('copies', 0))
+            );
     }
 
-    num(){
+    num():Observable<number>{
         var url = this.baseUrl + 'numb';
-        return this.http.get(this.location.prepareExternalUrl(url))
-                .map(res => <number>res.json() )
-                .catch(this.handleError);
-    }
-
-    getRequestOptions(){
-        return new RequestOptions(
-            {
-                headers: new Headers({
-                    "Content-Type": "application/json; charset=utf-8"
-                })
-            }
-        )
-    }
-
-    handleError(err:Response){
-        console.error(err);
-        return Observable.throw(err.json().error || 'Server error');
+        return this.http.get<number>(this.location.prepareExternalUrl(url))
+        .pipe(
+            catchError(this.handleError('num', 0))
+        );
     }
 }
 
