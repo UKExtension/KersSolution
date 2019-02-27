75a6987bcee8b61b8c3a0e46759cdebbeca580d3
Ivelin Denev <idenev@extension.org>
2019-02-27 11:15:02 -0500

    Cache Program Indicators Report

--- Kers/Controllers/Reports/ProgramIndicatorsController.cs
+++ Kers/Controllers/Reports/ProgramIndicatorsController.cs
@@ -69,7 +69,7 @@ namespace Kers.Controllers.Reports
                 
                 return new StatusCodeResult(500);
             }
-            var indicators = new List<StrategicInitiativeIndicatorsViewModel>();
+            
             if( id != 0){
                 var county = context.PlanningUnit.Find(id);
                 if(county == null){
@@ -79,76 +79,102 @@ namespace Kers.Controllers.Reports
             }else{
                 ViewData["county"] = "STATE TOTALS";
             }
-            var initiatives = await context.StrategicInitiative.Where(s => s.FiscalYear == fiscalYear)
-                                    .Include( s => s.ProgramCategory)
-                                    .Include( s => s.MajorPrograms)
-                                    .OrderBy( s => s.order)
-                                    .ToListAsync();
-            var indicatorsPerProgram = await context.ProgramIndicator
-                                                .GroupBy( p => p.MajorProgramId )
-                                                .Select( g => new {
-                                                    MajorProgramId = g.Key,
-                                                    Indicators = g.Select( d => d ).OrderBy( d => d.order)
-                                                }).ToListAsync();
-
-            var ReportingCountiesPerProgram = new List<ReportingCountiesPerProgram>();
-
-            foreach( var initiative in initiatives){
-                var intv = new StrategicInitiativeIndicatorsViewModel();
-                intv.Title = initiative.ProgramCategory.ShortName + " " + initiative.Name;
-                intv.Code = initiative.PacCode;
-                intv.Indicators = new List<MajorProgramIndicatorsViewModel>();
-                foreach(var program in initiative.MajorPrograms){
 
 
-                    if( id == 0 ){
-                        var allIndicators = context.ProgramIndicatorValue
-                                                .Where( v => v.ProgramIndicator.MajorProgram == program )
-                                                .GroupBy( v => v.PlanningUnit)
-                                                .Select( v => new {
-                                                    Unit = v.Key,
-                                                    Reported = v.Sum( l => l.Value )
-                                                });
-                        var reported = allIndicators.Where( i => i.Reported != 0).Select( s => s.Unit ).ToList();
-                        var reporting = new ReportingCountiesPerProgram();
-                        reporting.Units = reported;
-                        reporting.MajorProgram = program;
-                        reporting.UnitsToString = string.Join(", ", reporting.Units.OrderBy(s => s.Name).Select(x => x.Name.Substring(0, x.Name.Length - 11)));
-                        ReportingCountiesPerProgram.Add(reporting);
-                    }
+            var cacheKey = "ProgramIndicatorsReport" + fiscalYear.Name + "_" + id.ToString();
+            var cacheKeyCounties = "ProgramIndicatorsReportCountiesPerProgram" + fiscalYear.Name + "_" + id.ToString();
+            var cacheString = _cache.GetString(cacheKey);
+            var cacheStringCounties = _cache.GetString(cacheKeyCounties);
+            List<ReportingCountiesPerProgram> ReportingCountiesPerProgram;
+            List<StrategicInitiativeIndicatorsViewModel> indicators;
+            if (!string.IsNullOrEmpty(cacheString)){
+                indicators = JsonConvert.DeserializeObject<List<StrategicInitiativeIndicatorsViewModel>>(cacheString);
+                ReportingCountiesPerProgram = JsonConvert.DeserializeObject<List<ReportingCountiesPerProgram>>(cacheStringCounties);
+            }else{
 
-                    var programViewModel = new MajorProgramIndicatorsViewModel();
-                    programViewModel.Title = program.Name;
-                    programViewModel.Code = program.PacCode;
-                    programViewModel.Indicators = new List<IndicatorViewModel>();
-                    var indicatorsPerThisProgram = indicatorsPerProgram.Where( n => n.MajorProgramId == program.Id).FirstOrDefault();
-                    if( indicatorsPerThisProgram != null ){
-                        var i = 1;
-                        foreach( var indctr in indicatorsPerThisProgram.Indicators){
-                            var ind = new IndicatorViewModel();
-                            ind.Description = indctr.Question;
-                            ind.Code = i;
-                            if( id == 0){
-                                ind.Amount = context.ProgramIndicatorValue.Where( v => v.ProgramIndicatorId == indctr.Id ).Sum( d => d.Value);
-                            }else{
-                                ind.Amount = context.ProgramIndicatorValue
-                                                            .Where( v => 
-                                                                        v.ProgramIndicatorId == indctr.Id 
-                                                                            &&
-                                                                        v.PlanningUnitId == id
-                                                                        )
-                                                            .Sum( d => d.Value);
-                            }
-                            i++;
-                            programViewModel.Indicators.Add( ind );
+                indicators = new List<StrategicInitiativeIndicatorsViewModel>();
+
+
+
+
+                var initiatives = await context.StrategicInitiative.Where(s => s.FiscalYear == fiscalYear)
+                                        .Include( s => s.ProgramCategory)
+                                        .Include( s => s.MajorPrograms)
+                                        .OrderBy( s => s.order)
+                                        .ToListAsync();
+                var indicatorsPerProgram = await context.ProgramIndicator
+                                                    .GroupBy( p => p.MajorProgramId )
+                                                    .Select( g => new {
+                                                        MajorProgramId = g.Key,
+                                                        Indicators = g.Select( d => d ).OrderBy( d => d.order)
+                                                    }).ToListAsync();
+
+                ReportingCountiesPerProgram = new List<ReportingCountiesPerProgram>();
+
+                foreach( var initiative in initiatives){
+                    var intv = new StrategicInitiativeIndicatorsViewModel();
+                    intv.Title = initiative.ProgramCategory.ShortName + " " + initiative.Name;
+                    intv.Code = initiative.PacCode;
+                    intv.Indicators = new List<MajorProgramIndicatorsViewModel>();
+                    foreach(var program in initiative.MajorPrograms){
+
+
+                        if( id == 0 ){
+                            var allIndicators = context.ProgramIndicatorValue
+                                                    .Where( v => v.ProgramIndicator.MajorProgram == program )
+                                                    .GroupBy( v => v.PlanningUnit)
+                                                    .Select( v => new {
+                                                        Unit = v.Key,
+                                                        Reported = v.Sum( l => l.Value )
+                                                    });
+                            var reported = allIndicators.Where( i => i.Reported != 0).Select( s => s.Unit ).ToList();
+                            var reporting = new ReportingCountiesPerProgram();
+                            reporting.Units = reported;
+                            reporting.MajorProgram = program;
+                            reporting.UnitsToString = string.Join(", ", reporting.Units.OrderBy(s => s.Name).Select(x => x.Name.Substring(0, x.Name.Length - 11)));
+                            ReportingCountiesPerProgram.Add(reporting);
                         }
-                    }
-                    intv.Indicators.Add(programViewModel);
-                }
 
-                indicators.Add( intv );
+                        var programViewModel = new MajorProgramIndicatorsViewModel();
+                        programViewModel.Title = program.Name;
+                        programViewModel.Code = program.PacCode;
+                        programViewModel.Indicators = new List<IndicatorViewModel>();
+                        var indicatorsPerThisProgram = indicatorsPerProgram.Where( n => n.MajorProgramId == program.Id).FirstOrDefault();
+                        if( indicatorsPerThisProgram != null ){
+                            var i = 1;
+                            foreach( var indctr in indicatorsPerThisProgram.Indicators){
+                                var ind = new IndicatorViewModel();
+                                ind.Description = indctr.Question;
+                                ind.Code = i;
+                                if( id == 0){
+                                    ind.Amount = context.ProgramIndicatorValue.Where( v => v.ProgramIndicatorId == indctr.Id ).Sum( d => d.Value);
+                                }else{
+                                    ind.Amount = context.ProgramIndicatorValue
+                                                                .Where( v => 
+                                                                            v.ProgramIndicatorId == indctr.Id 
+                                                                                &&
+                                                                            v.PlanningUnitId == id
+                                                                            )
+                                                                .Sum( d => d.Value);
+                                }
+                                i++;
+                                programViewModel.Indicators.Add( ind );
+                            }
+                        }
+                        intv.Indicators.Add(programViewModel);
+                    }
+
+                    indicators.Add( intv );
+                }
+                _cache.SetString(cacheKey, JsonConvert.SerializeObject(indicators), new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromDays( this.getCacheSpan(fiscalYear) )
+                    });
+                _cache.SetString(cacheKeyCounties, JsonConvert.SerializeObject(ReportingCountiesPerProgram), new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromDays( this.getCacheSpan(fiscalYear) )
+                    });
             }
-            
             ViewData["fy"] = fiscalYear.Name;
             ViewData["id"] = id;
             ViewData["ReportingCountiesPerProgram"] = ReportingCountiesPerProgram;
@@ -190,6 +216,14 @@ namespace Kers.Controllers.Reports
                 fiscalYear = this.context.FiscalYear.Where( f => f.Name == fy && f.Type == type).FirstOrDefault();
             }
             return fiscalYear;
+        }
+        protected int getCacheSpan(FiscalYear fiscalYear){
+            int cacheDaysSpan = 350;
+            var today = DateTime.Now;
+            if(fiscalYear.Start < today && Math.Max( fiscalYear.End.Ticks, fiscalYear.ExtendedTo.Ticks) > today.Ticks){
+                cacheDaysSpan = 3;
+            }
+            return cacheDaysSpan;
         }
     }
 
