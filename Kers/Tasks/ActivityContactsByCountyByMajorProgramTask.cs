28ab2861637ee4d1b1960eeea5ae0df4c2d3599b
Ivelin Denev <ivelindenev@Ivelins-MacBook.local>
2019-10-03 17:49:29 -0400

    Get major programs for the fiscal year

--- Kers/Tasks/ActivityContactsByCountyByMajorProgramTask.cs
+++ Kers/Tasks/ActivityContactsByCountyByMajorProgramTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,7 +23,7 @@ namespace Kers.Tasks
         ){
             this.serviceProvider = serviceProvider;
         }
-        public string Schedule => "42 2 5 * *";
+        public string Schedule => "42 2 * * *";
         
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
@@ -36,17 +37,38 @@ namespace Kers.Tasks
                     var cache = scope.ServiceProvider.GetService<IDistributedCache>();
                     var fiscalYearRepo = new FiscalYearRepository( context );
                     var repo = new ActivityRepository(cache, context);
+                    var contactRepo = new ContactRepository(cache, context);
+                    
+                    var counties = context.PlanningUnit.Where( c => c.Name.Substring(c.Name.Count() - 3) == "CES");
+                    var currnetfiscalYear = fiscalYearRepo.currentFiscalYear(FiscalYearType.ServiceLog);
                     var startTime = DateTime.Now;
-                    var str = await repo.ContactsByCountyByMajorProgram(fiscalYearRepo.previoiusFiscalYear(FiscalYearType.ServiceLog), true);
+
+                    var str = new List<PlanningUnit>();
+                    //Rebuild summary cache
+                    Random rnd = new Random();
+                    foreach( var county in counties){
+                        int RndInt = rnd.Next(1, 3);
+                        if(RndInt == 1){
+                            var summary = await contactRepo.GetPerPeriodSummaries(currnetfiscalYear.Start, currnetfiscalYear.End,1,county.Id,true);
+                            str.Add(county);
+                        }
+                    }
+                    int r = rnd.Next(1, 10);
+                    if(r == 1){
+                        var s = await contactRepo.GetPerPeriodSummaries(currnetfiscalYear.Start, currnetfiscalYear.End,4,0,true);
+                    } 
+                    
+                    //r = rnd.Next(1, 100);
+                    //var str = await repo.ContactsByCountyByMajorProgram(fiscalYearRepo.previoiusFiscalYear(FiscalYearType.ServiceLog), true);
                     var endTime = DateTime.Now;
                     await LogComplete(context, 
-                                    "TableViewModel", str, 
-                                    "Activity Contacts By County By Major Program Task executed for " + (endTime - startTime).TotalSeconds + " seconds"
+                                    "Planning Unit", str, 
+                                    "Per Period Summaries Task executed for " + (endTime - startTime).TotalSeconds + " seconds"
                                 );
                 }catch( Exception e){
                     await LogError(context, 
                                     "ActivityContactsByCountyByMajorProgramTask", e, 
-                                    "Activity Contacts By County By Major Program Task failed"
+                                    "Per Period Summaries Task failed"
                             );
                 }
                 
