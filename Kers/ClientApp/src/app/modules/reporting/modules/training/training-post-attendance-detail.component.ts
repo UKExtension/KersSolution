afe77150218b3e986269e49498e6d752e6da58a2
Denev <idene3@uky.edu>
2020-03-08 17:59:59 -0400

    remove first row of the csv

--- Kers/ClientApp/src/app/modules/reporting/modules/training/training-post-attendance-detail.component.ts
+++ Kers/ClientApp/src/app/modules/reporting/modules/training/training-post-attendance-detail.component.ts
@@ -75,7 +75,7 @@ export class TrainingPostAttendanceDetailComponent implements OnInit {
     const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
     const header = Object.keys(data[0]);
     let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
-    csv.unshift(header.join(','));
+    //csv.unshift(header.join(','));
     let csvArray = csv.join('\r\n');
     var blob = new Blob([csvArray], {type: 'text/csv' })
     saveAs(blob, filename);
