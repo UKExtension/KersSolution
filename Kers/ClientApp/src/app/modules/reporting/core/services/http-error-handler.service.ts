929358c6971c3fcbb260c5b161c6c225000b3c0c
Ivelin Denev <ivelindenev@Ivelins-MacBook.local>
2020-04-10 16:34:00 -0400

    Put error message on the right place

--- Kers/ClientApp/src/app/modules/reporting/core/services/http-error-handler.service.ts
+++ Kers/ClientApp/src/app/modules/reporting/core/services/http-error-handler.service.ts
@@ -44,7 +44,8 @@ export class HttpErrorHandler {
       // TODO: better job of transforming error for user consumption
 
       var log = <Log>{};
-      log.description = `${serviceName}: ${operation} failed: ${message}`;
+      log.object = `${serviceName}: ${operation} failed: ${message}`;
+      log.description = `${serviceName}: ${operation} failed`;
       this.http.post<Log>(this.location.prepareExternalUrl('/api/log/'), log).subscribe();
       this.messageService.add(log.description);
       // Let the app keep running by returning a safe result.
