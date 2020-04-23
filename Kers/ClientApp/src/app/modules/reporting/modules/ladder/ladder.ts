7c0cb7fbda85a8b3dfbb81b27e05d9b212b519e0
Denev <idene3@uky.edu>
2020-04-23 13:13:45 -0400

    Ladder Application ts class

---
+++ Kers/ClientApp/src/app/modules/reporting/modules/ladder/ladder.ts
@@ -0,0 +1,10 @@
+import { User } from '../user/user.service';
+
+export class LadderApplication{
+    id:number;
+    kersUser:User;
+    kersUserId:number;
+    positionNumber:string;
+    lastPromotion:Date;
+
+}
