f22d5634a8260c3adceaf058df31f85712cb44d8
idenev <idengo@gmail.com>
2021-11-18 16:58:46 -0500

    Add example major program survey

---
+++ Kers/ClientApp/src/app/modules/reporting/modules/training/survey/outline_example.ts
@@ -0,0 +1,162 @@
+export const outlineJson = {
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "checkbox",
+        "name": "How did you access Extension information on swine production during the past 12 months?",
+        "description": "Select all that apply.",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Newsletter"
+         },
+         {
+          "value": "item2",
+          "text": "Program/Meeting"
+         },
+         {
+          "value": "item3",
+          "text": "Farm visit"
+         },
+         {
+          "value": "item4",
+          "text": "Phone Call, email or office visit"
+         },
+         {
+          "value": "item5",
+          "text": "Website"
+         },
+         {
+          "value": "item6",
+          "text": "Social media"
+         }
+        ],
+        "hasOther": true,
+        "colCount": 2
+       },
+       {
+        "type": "checkbox",
+        "name": "Which Extension diagnostic services did you utilize related to swine production during the past 12 months?",
+        "description": "Select all that apply.",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Soil test"
+         },
+         {
+          "value": "item2",
+          "text": "Manure analysis"
+         },
+         {
+          "value": "item3",
+          "text": "Animal disease"
+         }
+        ],
+        "colCount": 2
+       },
+       {
+        "type": "radiogroup",
+        "name": "Did the diagnostic information influence the decisions that you made in treating soil, weeds, invasive species, or pests?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ],
+        "colCount": 2
+       },
+       {
+        "type": "radiogroup",
+        "name": "Did the diagnostic information influence the decisions that you made related to animal nutrition or health?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ],
+        "colCount": 2
+       },
+       {
+        "type": "matrix",
+        "name": "In the past 12 months, which water and soil quality and/or conservation practices did you use?",
+        "columns": [
+         "Yes",
+         "No"
+        ],
+        "rows": [
+         "Implemented one or more best practices for soil/water quality",
+         "Developed or updated an Ag Water Quality Plan",
+         "Developed or implemented nutrient management plan",
+         "Adjusted rate, timing, placement and/or sources to increase nutrient use efficiency",
+         "Implemented one or more best practices to increase ground cover for soil protection",
+         "Implemented one or more best practices for improving water quality",
+         "Other"
+        ]
+       },
+       {
+        "type": "text",
+        "name": "Comments on soil quality and/or conservation practices you use"
+       },
+       {
+        "type": "matrix",
+        "name": "In the past 12 months, did you adopt or improve the following practices?",
+        "columns": [
+         "Yes",
+         "No"
+        ],
+        "rows": [
+         "Improved or maintained record keeping practices",
+         "Incorporated technology to effectively manage farm operations",
+         "Applied marketing techniques for meat animals",
+         "Adopted new technologies in agriculture production",
+         "Adapted practices or equipment for safety",
+         "Improved equipment or facilities",
+         "Increased profits, reduced expenses and/or reduced risk",
+         "Other"
+        ]
+       },
+       {
+        "type": "text",
+        "name": "Comments on improving Farm Management and Economics practices."
+       },
+       {
+        "type": "matrix",
+        "name": "How have you been a leader for agriculture and the environment during the past 12 months?",
+        "columns": [
+         "Yes",
+         "No"
+        ],
+        "rows": [
+         "Shared information about agriculture or the environment issues locally",
+         "Advocated for agriculture and/or environmental issues locally",
+         "Shared information about agriculture and/or environment issues with elected officials",
+         "Advocates for agriculture and/or environmental issues with elected officials",
+         "Advised, assisted or mentored a new or beginning agriculture producer",
+         "Served as a member/leader of advisory councils, community development committee and/or leadership role with an agriculture organization",
+         "Other"
+        ]
+       },
+       {
+        "type": "text",
+        "name": "Comments on Ag Extension Leadership"
+       },
+       {
+        "type": "text",
+        "name": "Share your thoughts and suggestions to improve future programs and services."
+       }
+      ],
+      "title": "Swine Evaluation",
+      "description": "Extension Service takes pride in providing programs and resources to benefit the livestock and agriculture operations. In order to serve you, our staff would appreciate you taking a few minutes to complete this survey."
+     }
+    ]
+   }
