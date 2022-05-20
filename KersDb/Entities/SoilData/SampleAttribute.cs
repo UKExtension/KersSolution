fc841bb2d7050d5a7bc8c4fac79562b6ba6e8d0c
Ivelin Denev <idenev@extension.org>
2022-05-20 18:28:02 -0400

    Insert initial data

--- KersDb/Entities/SoilData/SampleAttribute.cs
+++ KersDb/Entities/SoilData/SampleAttribute.cs
@@ -10,6 +10,6 @@ namespace Kers.Models.Entities.SoilData
         public int Id { get; set; }
         public string Name {get;set;}
         public int SampleAttributeTypeId {get;set;}
-        //public SampleAttributeType SampleAttributeType {get;set;}
+        public SampleAttributeType SampleAttributeType {get;set;}
     }
 }
--- KersDb/Entities/SoilData/SampleAttributeType.cs
+++ KersDb/Entities/SoilData/SampleAttributeType.cs
@@ -8,9 +8,9 @@ namespace Kers.Models.Entities.SoilData
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Label {get;set;}
+        public string Name {get;set;}
         public int TypeFormId {get;set;}
-        //public TypeForm TypeForm {get;set;}
+        public TypeForm TypeForm {get;set;}
         public int Order {get;set;}
     }
 }
