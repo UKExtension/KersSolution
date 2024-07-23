edb1b845e7335c35d562717b1395ae6eeef86e54
Ivelin Denev <idengo@gmail.com>
2024-07-23 10:06:40 -0400

    Display KSU stories

--- Kers/Controllers/Reports/StoryController.cs
+++ Kers/Controllers/Reports/StoryController.cs
@@ -119,6 +119,97 @@ namespace Kers.Controllers.Reports
         }
 
 
+
+
+
+
+
+
+        [HttpGet]
+        [Route("ksu/{fy?}")]
+        public async Task<IActionResult> Ksu(
+            string currentFilter,
+            string searchString,
+            int? page,
+            string sortOrder = "newest",
+            int planningUnitId = 0,
+            int programId = 0,
+            int length = 18,
+            string fy="0")
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["CurrentLength"] = length;
+            ViewData["Units"] = this.context.PlanningUnit.OrderBy( u => u.order).ToList();
+            ViewData["Program"] = this.context.MajorProgram.Where( p => p.StrategicInitiative.FiscalYear.Name == fy).OrderBy( u => u.order).ToList();
+
+            ViewBag.CurrentUnit = planningUnitId;
+            ViewBag.CurrentProgram = programId;
+
+            if (searchString != null)
+            {
+                page = 1;
+            }else{
+                searchString = currentFilter;
+            }
+            if( fy == "0"){
+                var fiscalYear = this.GetFYByName(fy);
+                fy = fiscalYear.Name;
+            }
+            ViewData["CurrentFilter"] = searchString;
+
+            var stories = from s in context.Story
+                        select s;
+            
+
+            stories = stories.Where( s => s.MajorProgram.StrategicInitiative.FiscalYear.Name == fy);
+
+            if(programId != 0 ){
+                stories = stories.Where( u => u.MajorProgramId == programId );
+            }
+            
+            
+            switch (sortOrder)
+            {
+
+                case "oldest":
+                    stories = stories.OrderBy(s => s.Updated);
+                    break;
+                case "author":
+                    stories = stories
+                            .OrderBy(s => s.KersUser.PersonalProfile.FirstName)
+                            .ThenBy(s => s.KersUser.PersonalProfile.LastName);
+                    break;
+                default:
+                    stories = stories.OrderByDescending(s => s.Updated);
+                    break;
+            }
+            int pageSize = length;
+            stories = stories.Include( u => u.Revisions ).ThenInclude( p => p.StoryImages).ThenInclude( p => p.UploadImage).ThenInclude( i => i.UploadFile )
+                        .Include(u => u.KersUser.PersonalProfile)
+                        .Include( u => u.MajorProgram);
+
+            var list = await PaginatedList<Story>.CreateAsync(stories.AsNoTracking(), page ?? 1, pageSize);
+            ViewData["fy"] = fy;
+            ViewData["FiscalYear"] = GetFYByName(fy);
+
+            return View(list);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         [HttpGet]
         [Route("astable/{fy?}")]
         public async Task<IActionResult> AsTable(
