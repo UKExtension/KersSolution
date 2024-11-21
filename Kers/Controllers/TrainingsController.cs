2d3da6c9b8480b65a04e6a2209750db18670cd6c
Ivelin Denev <idengo@gmail.com>
2024-11-21 15:27:21 -0500

    Do not change date if something is wrong with it

--- Kers/Controllers/TrainingsController.cs
+++ Kers/Controllers/TrainingsController.cs
@@ -403,13 +403,15 @@ namespace Kers.Controllers
                             .FirstOrDefault();
             if(training != null && trn != null ){
                 var isMovedToCatalog = false;
-                trn.TrainingSession = new List<TrainingSession>();
-                foreach( var s in training.TrainingSessionWithTimes){
-                    trn.TrainingSession.Add( s.GetSession(training.Etimezone));
+                if(training.TrainingSessionWithTimes.First().Date.Year > 2000 ){
+                    trn.TrainingSession = new List<TrainingSession>();
+                    foreach( var s in training.TrainingSessionWithTimes){
+                        trn.TrainingSession.Add( s.GetSession(training.Etimezone));
+                    }
+                    var sortedSessions = trn.TrainingSession.OrderBy( s => s.Start);
+                    trn.Start = sortedSessions.First().Start;
+                    trn.End = sortedSessions.Last().End;
                 }
-                var sortedSessions = trn.TrainingSession.OrderBy( s => s.Start);
-                trn.Start = sortedSessions.First().Start;
-                trn.End = sortedSessions.Last().End;
                 trn.Subject = training.Subject;
                 trn.Body = training.Body;
                 trn.tLocation = training.tLocation;
