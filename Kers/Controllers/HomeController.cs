fb1f9fc52fefe0a7db9e5254f25fb433944eb4a1
Ivelin Denev <idengo@gmail.com>
2025-08-16 08:06:33 -0400

    Cookie with sso

--- Kers/Controllers/HomeController.cs
+++ Kers/Controllers/HomeController.cs
@@ -95,7 +95,7 @@ namespace Kers.Controllers
         [AllowAnonymous]
         [HttpPost]
         [Route("auth")]
-        public IActionResult Auth( [FromForm]LoginViewModel loginViewModel)
+        public async Task<IActionResult> Auth( [FromForm]LoginViewModel loginViewModel)
         {
 
             string samlCertificate = @"-----BEGIN CERTIFICATE-----
@@ -213,6 +213,52 @@ namespace Kers.Controllers
                     qb.Add( "access_token", new JwtSecurityTokenHandler().WriteToken(token));
                     if(!String.IsNullOrEmpty(relayState)) qb.Add("relayState", relayState);
                     var url = red + qb;
+
+
+
+
+                    var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var authProperties = new AuthenticationProperties
+                    {
+                        //AllowRefresh = <bool>,
+                        // Refreshing the authentication session should be allowed.
+
+                        //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                        // The time at which the authentication ticket expires. A 
+                        // value set here overrides the ExpireTimeSpan option of 
+                        // CookieAuthenticationOptions set with AddCookie.
+
+                        //IsPersistent = true,
+                        // Whether the authentication session is persisted across 
+                        // multiple requests. When used with cookies, controls
+                        // whether the cookie's lifetime is absolute (matching the
+                        // lifetime of the authentication ticket) or session-based.
+
+                        //IssuedUtc = <DateTimeOffset>,
+                        // The time at which the authentication ticket was issued.
+
+                        //RedirectUri = <string>
+                        // The full path or absolute URI to be used as an http 
+                        // redirect response value.
+                    };
+
+
+
+                    await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity),
+                        authProperties);
+
+
+
+
+
+
+
+
+
                     return Redirect(url);
                 }catch(Exception ex){
                     Log(   null, 
